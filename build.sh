e81c65065d00b097518f26d10151a96e20e84af3
Márton Elek <elek@apache.org>
2022-03-08 15:38:40 +0100

    switch to the latest registry commit

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker run --name registry -d -p 5000:5000 -e UPLINK_ACCESS=$UPLINK_ACCESS -e UPLINK_BUCKET=registry2 ghcr.io/elek/distribution:mniewrzal-d919f5f4
+docker run --name registry -d -p 5000:5000 -e UPLINK_ACCESS=$UPLINK_ACCESS -e UPLINK_BUCKET=registry2 ghcr.io/elek/distribution:mniewrzal-80ce0748
 sleep 10
 time docker pull localhost:5000/elek/1g
 docker stop registry
