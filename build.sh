5c720d451007dba55bb61ecca1ac8997c945b598
Márton Elek <elek@apache.org>
2022-03-08 20:28:09 +0100

    use urandom as a pull source

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@ time docker pull localhost:5000/elek/1g
 dd if=/dev/urandom of=100mb count=102400 bs=1024
 IMAGE=localhost:5000/elek/1g:`date +%s`
 docker build -t $IMAGE .
-docker push $IMAGE
+time docker push $IMAGE
 
 docker stop registry
