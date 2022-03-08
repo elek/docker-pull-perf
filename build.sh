130df51ed84706b68ace58119d122706b4f7f748
Márton Elek <elek@apache.org>
2022-03-08 15:19:12 +0100

    test docker hub pull speed

--- build.sh
+++ build.sh
@@ -1,4 +1,1 @@
-docker run --name registry -d -p 5000:5000 -e UPLINK_ACCESS=$UPLINK_ACCESS -e UPLINK_BUCKET=registry2 ghcr.io/elek/distribution:mniewrzal-d919f5f4
-sleep 10
-time docker pull localhost:5000/elek/1g
-docker stop
+time docker pull elek/1g
