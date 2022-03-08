593386fa1ccb478301a6f94fb3ac67dade3b5c67
Márton Elek <elek@apache.org>
2022-03-08 20:32:22 +0100

    error handling

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/env bash
+set -ex
+
 docker run --name registry -d -p 5000:5000 -e UPLINK_ACCESS=$UPLINK_ACCESS -e UPLINK_BUCKET=registry2 ghcr.io/elek/distribution:mniewrzal-80ce0748
 sleep 10
 time docker pull localhost:5000/elek/1g
 
 dd if=/dev/urandom of=100mb count=102400 bs=1024
-IMAGE=localhost:5000/elek/1g:`date +%s`
+export IMAGE=localhost:5000/elek/1g:`date +%s`
 docker build -t $IMAGE .
 time docker push $IMAGE
 
