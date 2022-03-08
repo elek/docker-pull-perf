b65ca3bd56a3b3c78117503fee1e110bb5f9ff89
Márton Elek <elek@apache.org>
2022-03-08 15:18:00 +0100

    test pull speed from storj

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
-dd if=/dev/urandom of=100mb count=102400 bs=1024
-docker build -t ghcr.io/elek/1g .
-docker push ghcr.io/elek/1g
+docker run --name registry -d -p 5000:5000 -e UPLINK_ACCESS=$UPLINK_ACCESS -e UPLINK_BUCKET=registry2 ghcr.io/elek/distribution:mniewrzal-d919f5f4
+sleep 10
+time docker pull localhost:5000/elek/1g
+docker stop
